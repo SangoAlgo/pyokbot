f3acb8b56180b69b1ac04c7b446c82111fb1d593
Serhio Bergher <sangoaligo@gmail.com>
2026-07-25 08:28:05 +0300

    chore: bump to 1.0.1 for README update (OK.ru avatar, remove alpha warning)

--- pyokbot/__init__.py
+++ pyokbot/__init__.py
@@ -10,7 +10,7 @@ from .errors import (
 from .logging_config import logger
 from .opcodes import MessageOpcode
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = [
     "Vanus",
     "MessageOpcode",
